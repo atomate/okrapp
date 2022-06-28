43b25d36a0114b5ea29c8a0dd4b75f8f606305c4
Cristian <sellercsgo@gmail.com>
2022-06-28 20:26:21 +0300

    Fixed show view for key results:

--- project/resources/views/keyresult/show.blade.php
+++ project/resources/views/keyresult/show.blade.php
@@ -10,11 +10,11 @@
             @foreach($objectives as $objective)
                 <div class="col-12 d-flex align-items-center">
                     <h1 class="text-center">{{$objective->name}}</h1>
-                    <h2 class="text-center mt-1">-{{round($objective->keyResult->avg('progress'))}}%</h2>
+                    <h2 class="text-center mt-1">-{{round($objective->keyResults->avg('progress'))}}%</h2>
                 </div>
                 <div class="col-12">
                     <ul>
-                        @foreach($objective->keyResult as $keyResult)
+                        @foreach($objective->keyResults as $keyResult)
                             <form action="{{route('key-result.destroy',$keyResult)}}" method="post">
                                 @csrf
                                 @method('delete')
