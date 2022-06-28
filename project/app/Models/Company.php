293682ebf8c451538d109965d51b3499976bc881
Cristian <sellercsgo@gmail.com>
2022-06-28 14:31:50 +0300

    Company model edited

--- project/app/Models/Company.php
+++ project/app/Models/Company.php
@@ -10,7 +10,7 @@ class Company extends Model
     use HasFactory;
 
     protected $table = "companies";
-    protected $fillable = ['title'];
+    protected $fillable = ['name'];
 
     public function objective()
     {
