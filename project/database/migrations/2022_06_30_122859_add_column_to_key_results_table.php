07a93282eadc8f576bae6d4a8218d6a50d323a78
Cristian <sellercsgo@gmail.com>
2022-06-30 15:34:00 +0300

    index for objective_id in key results table

---
+++ project/database/migrations/2022_06_30_122859_add_column_to_key_results_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('key_results', function (Blueprint $table) {
+            $table->foreign('objective_id')
+                ->references('id')
+                ->on('objective')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('key_results', function (Blueprint $table) {
+            $table->dropForeign('objective_id');
+        });
+    }
+};
