4f4342978e08275211c82bc7f871539986ca9f62
Cristian <sellercsgo@gmail.com>
2022-07-05 22:16:13 +0300

    Edited api.php

--- project/routes/api.php
+++ project/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('api/companies',\App\Http\Controllers\ApiCompanyController::class);
+Route::resource('companies',\App\Http\Controllers\ApiCompanyController::class);
